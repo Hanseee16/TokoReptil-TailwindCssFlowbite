26e42853344b9fb59ae7688c2c54212f499b51ff
Hanseee16 <kamilfarhan223@gmail.com>
2023-04-16 23:38:55 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  darkMode: "media",
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
