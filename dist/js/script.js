36ff69d9a260a5b1db950f83e9e0af2a164c894f
Hanseee16 <kamilfarhan223@gmail.com>
2023-04-20 02:13:47 +0700

    belom kelar cuy

--- dist/js/script.js
+++ dist/js/script.js
@@ -100,3 +100,28 @@ function easeInOutCubic(t, b, c, d) {
   t -= 2;
   return (c / 2) * (t * t * t + 2) + b;
 }
+
+// nav link active
+// 1. tangkap element dengan class menu
+const menu = document.querySelector(".menu");
+
+// 2. jika element dengan class menu diklik
+menu.addEventListener("click", function (e) {
+  // 3. maka ambil element apa yang diklik oleh user
+  const targetMenu = e.target;
+
+  // 4. lalu cek, jika element itu adalah link dengan class menu__link
+  if (targetMenu.classList.contains("menu__link")) {
+    // 5. maka ambil menu link yang aktif
+    const menuLinkActive = document.querySelector("ul li a.active");
+
+    // 6. lalu cek, Jika menu link active ada dan menu yang di klik user adalah menu yang tidak sama dengan menu yang aktif, (cara cek-nya yaitu dengan membandingkan value attribute href-nya)
+    if (menuLinkActive !== null && targetMenu.getAttribute("href") !== menuLinkActive.getAttribute("href")) {
+      // 7. maka hapus class active pada menu yang sedang aktif
+      menuLinkActive.classList.remove("active");
+    }
+
+    // 8. terakhir tambahkan class active pada menu yang di klik oleh user
+    targetMenu.classList.add("active");
+  }
+});
